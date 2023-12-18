36d2012f5ec52fd1f07d659a42c5c9c499980693
Peter Francis Robante <robantepeterfran@gmail.com>
2023-12-18 22:22:02 +0800

    chore:clean up and setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,31 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      phone: "412px", // => @media (min-width: 412px)
+      tablet: "576px", // => @media (min-width: 576px)
+      laptop: "960px", // => @media (min-width: 960px)
+      desktop: "1480px", // => @media (min-width: 1280px)
+    },
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#A11E22",
+          secondary: "#1DIE23",
+          accent: "#E5E4DA",
+          neutral: "#1B1A1A",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#36D399",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+
+  plugins: [require("daisyui")],
 };
